f0fcbdfac676d8c0055f64ea036836620c22b6cc
Ranjit Thota <amailtoranjith@gmail.com>
2023-05-07 00:45:58 +0200

    date input in settings

--- ScreensForProfile/Settings.js
+++ ScreensForProfile/Settings.js
@@ -22,6 +22,7 @@ import DateTimePicker from "@react-native-community/datetimepicker";
 const Settings = () => {
   const [name, setName] = useState("Ranjit");
   const [city, setCity] = useState("Rodez");
+  const [dobBefore, setDobBeforeFormat] = useState("");
   const [dob, setDob] = useState("18-07-1994");
   const [profilePhoto, setProfilePhoto] = useState(null);
   const [photo, setPhoto] = useState(null);
@@ -33,92 +34,33 @@ const Settings = () => {
 
   const [date, setDate] = useState(new Date());
 
-  useEffect(() => {
-    (async () => {
-      if (Platform.OS !== "web") {
-        const { status } =
-          await ImagePicker.requestMediaLibraryPermissionsAsync();
-        if (status !== "granted") {
-          alert("Sorry, we need camera roll permissions to make this work!");
-        }
-      }
-    })();
-  }, []);
+  function showDatePicker() {
+    setDatePicker(true);
+  }
 
-  const pickImage = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
+  function onDateSelected(event, value) {
+    setDate(value);
+    const date = new Date(value);
+    const formatter = new Intl.DateTimeFormat("en-UK", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
     });
+    const formattedDate = formatter.format(date).split("/").join("-");
+    setDob(formattedDate);
 
-    console.log(result);
-
-    if (!result.cancelled) {
-      setSelectedImage(result.uri);
-    }
-  };
-
-  // console.log(currUser);
-  // console.log(profilePhoto);
-
-  // const handleNameChange = (e) => {
-  //   setName(e.target.value);
-  // };
-
-  // const handleCityChange = (e) => {
-  //   setCity(e.target.value);
-  // };
-
-  // const handleDobChange = (date) => {
-  //   setDob(date);
-  // };
-
-  // const handleProfilePhotoChange = (e) => {
-  //   setProfilePhoto(e.target.files[0]);
-  // };
-  const renderPhoto = () => {
-    return (
-      <Text style={{ fontSize: 16, marginTop: 10 }}>
-        Veuillez choisir une photo
-      </Text>
-    );
-  };
-
-  const options = {
-    mediaType: "photo",
-    includeBase64: true,
-    maxHeight: 500,
-    maxWidth: 500,
-  };
-
-  const handleChoosePhoto = () => {
-    const options = {
-      mediaType: "photo",
-      maxWidth: 300,
-      maxHeight: 300,
-      quality: 0.5,
-      includeBase64: true,
-    };
-    ImagePicker.launchImageLibrary(options, (response) => {
-      if (response.didCancel) {
-        console.log("User cancelled image picker");
-      } else if (response.error) {
-        console.log("ImagePicker Error: ", response.error);
-      } else {
-        setPhoto(response);
-      }
-    });
-  };
+    setDatePicker(false);
+    // console.log(value);
+  }
 
   return (
-    <View style={styles.profileSettings}>
+    <View style={styles.container}>
       <View style={styles.profileSetting}>
         <Text style={styles.label}>Email:</Text>
         <TextInput
-          style={styles.input}
+          style={[styles.input]}
           // value={currUser.email}
+          value="amailtoranjith@gmail.com"
           editable={false}
         />
       </View>
@@ -140,32 +82,26 @@ const Settings = () => {
           placeholder="Ville"
         />
       </View>
+
       <View style={styles.profileSetting}>
         <Text style={styles.label}>Date de naissance:</Text>
-        <TextInput
-          style={styles.input}
-          // value={city}
-          // onChangeText={handleCityChange}
-          placeholder="Date"
-        />
-        {/* <DatePicker
-          style={styles.datePicker}
-          date={dob}
-          // onDateChange={handleDobChange}
-          mode="date"
-          placeholder="Date de naissance"
-          format="DD/MM/YYYY"
-          showIcon={false}
-        /> */}
+        <TouchableOpacity onPress={showDatePicker}>
+          <Text style={styles.input}>{dob && dob}</Text>
+        </TouchableOpacity>
       </View>
-      <Text>Hey</Text>
-
-      <View style={styles.imagecontainer}>
-        <Button title="Pick an image from camera roll" onPress={pickImage} />
-        {selectedImage && (
-          <Image source={{ uri: selectedImage }} style={styles.image} />
+      <View style={styles.profileSetting}>
+        {datePicker && (
+          <DateTimePicker
+            value={date}
+            mode={"date"}
+            display={Platform.OS === "ios" ? "spinner" : "default"}
+            is24Hour={true}
+            onChange={onDateSelected}
+            style={styles.datePicker}
+          />
         )}
       </View>
+
       <TouchableOpacity
         style={styles.button}
         // onPress={handleSaveChanges}
@@ -182,6 +118,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  profileSetting: {
+    marginHorizontal: 20,
+    width: "80%",
+    maxHeight: 50,
+    marginVertical: 20,
   },
   mainHeading: {
     fontSize: 24,
@@ -218,6 +162,33 @@ const styles = StyleSheet.create({
     width: 200,
     height: 200,
     resizeMode: "contain",
+  },
+
+  MainContainer: {
+    // flex: 1,
+    padding: 6,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "white",
+    // width: 400,
+    flexDirection: "row",
+  },
+
+  text: {
+    fontSize: 25,
+    color: "red",
+    padding: 3,
+    marginBottom: 10,
+    textAlign: "center",
+  },
+
+  // Style for iOS ONLY...
+  datePicker: {
+    justifyContent: "center",
+    alignItems: "flex-start",
+    width: 320,
+    height: 260,
+    display: "flex",
   },
 });
 
